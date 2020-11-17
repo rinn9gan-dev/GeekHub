9480a9c6a19ad827c1b271538ad52c0c92704b9d
Yaroslav Smoliar <yaroslav.smoliar.dev@gmail.com>
2020-11-17 17:36:15 +0200

    homework 2

--- regexp/index.js
+++ regexp/index.js
@@ -1,8 +1,31 @@
 document.querySelector('#user-form').addEventListener('submit', function (e) {
 	e.preventDefault();
 
+	const fullName = document.getElementById('name');
 
+	if (fullName.value && isValidFullName(fullName.value)) {
+		fullName.style.backgroundColor = '#5CEC5C';
+	} else {
+		fullName.style.backgroundColor = '#F9D0C4';
+	}
+
+	const email = document.getElementById('email');
+
+	if (email.value && isValidEmail(email.value)) {
+		email.style.backgroundColor = '#5CEC5C';
+	} else {
+		email.style.backgroundColor = '#F9D0C4';
+	}
+
+	const password = document.getElementById('password');
+
+	if (password.value && isValidPassword(password.value)) {
+		password.style.backgroundColor = '#5CEC5C';
+	} else {
+		password.style.backgroundColor = '#F9D0C4';
+	}
 });
+
 
 document.querySelectorAll('[data-show]').forEach(function (button) {
 	button.addEventListener('click', function (e) {
@@ -12,3 +35,20 @@ document.querySelectorAll('[data-show]').forEach(function (button) {
 		document.querySelector('#' + e.currentTarget.getAttribute('data-show')).classList.remove('d-none');
 	});
 });
+
+
+
+const isValidFullName = (fullName) => {
+	const nameRules = /([абвгґдеєжзиіїйклмнопрстуфхцчшщьюя]+)/gi;
+	return fullName.match(nameRules).length === 3;
+};
+
+const isValidEmail = (email) => {
+	const emailRules = /[^_.-]([a-z\d-.]+)[^_.-]@[^_.-]([a-z]+)\.([a-z]+)$/gmi;
+	return emailRules.test(email);
+};
+
+const isValidPassword = (password) => {
+	const passwordRules = /(?=^.{8,}$)(?=.*[0-9])(?=.*[A-Z])(?=.*[A-Z])/gm
+	return passwordRules.test(password);
+};
