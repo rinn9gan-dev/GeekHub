8c0577d79ae305edb50d5df18eb3f5427e5b6d95
Yaroslav Smoliar <yaroslav.smoliar.dev@gmail.com>
2021-01-05 14:58:26 +0200

    homework 3 / DOM

--- dom/index.js
+++ dom/index.js
@@ -4,7 +4,15 @@ jQuery('input').on('paste', function (e) {
 	var text = e.originalEvent.clipboardData.getData('text/plain');
 	var input = e.currentTarget;
 
+	removeAllChildNodes(table);
 
+	const arrayText = processText(text);
+	const tableHead = createThead(arrayText);
+	const tableBody = createTbody(arrayText);
+
+	table.appendChild(tableHead);
+	table.appendChild(tableBody);
+	console.log(arrayText);
 });
 
 var currentColumn;
@@ -45,3 +53,76 @@ jQuery('#column-menu [data-action]').on('click', function (e) {
 
 	jQuery('#column-menu').removeClass('d-block');
 });
+
+const processText = (text) => {
+	const splitByLines = text.split('\n');
+	return splitByLines.map(line => line.split(';'));
+}
+
+const table = document.getElementById('table');
+
+// const createTable = (table, arrayText) => {
+// 	for (let i = 0; i < arrayText.length; i++) {
+// 		const tr = document.createElement('tr');
+//
+// 		for (let j = 0; j < arrayText[i].length; j++){
+// 			const td = document.createElement('td');
+// 			td.innerHTML = arrayText[i][j];
+//
+// 			tr.appendChild(td);
+// 		}
+// 		table.appendChild(tr);
+// 	}
+// };
+
+const removeAllChildNodes = () => {
+	while (table.firstChild) {
+		table.removeChild(table.firstChild);
+	}
+};
+
+const createThead = (arrayText) => {
+	const abetka = ['A','B','C','D','E','F','G'];
+	const tHead = document.createElement('thead');
+	const tR = document.createElement('tr');
+	const tH = document.createElement('th');
+
+	tH.textContent = ' ';
+	tR.appendChild(tH);
+
+	arrayText[0].forEach((_, index) => {
+		const tH = document.createElement('th');
+		tH.textContent = abetka[index];
+		tR.appendChild(tH);
+	});
+
+	tHead.appendChild(tR);
+
+	return tHead;
+}
+
+const createTbody = (arrayText) => {
+	const tBody = document.createElement('tbody');
+	
+	arrayText.forEach((row, index) => {
+		const tR = document.createElement('tr');
+		const tH = document.createElement('th');
+		tH.textContent = index + 1;
+
+		tR.appendChild(tH);
+
+		row.forEach((value) => {
+			const tD = document.createElement('td');
+			const input = document.createElement('input');
+			input.value = value;
+
+			tD.appendChild(input);
+			tR.appendChild(tD);
+		});
+
+		tBody.appendChild(tR);
+	});
+
+
+	return tBody;
+}
